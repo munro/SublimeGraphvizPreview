cb2af11e089e988e85df90c641373d2330e48d00
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2014-05-22 22:15:52 +0200

    Preview for DOT files

--- GraphvizPreview.py
+++ GraphvizPreview.py
@@ -8,23 +8,15 @@ try:  # python 3
 except ValueError:  # python 2
     from helpers import surroundingGraphviz, graphvizPDF, ENVIRON
 
+DOT_SYNTAX = 'Packages/Graphviz/DOT.tmLanguage'
 
 class GraphvizPreviewCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        sel = self.view.sel()[0]
-
-        if not sel.empty():
-            code = self.view.substr(sel).strip()
-        else:
-            code = surroundingGraphviz(
-                self.view.substr(sublime.Region(0, self.view.size())),
-                sel.begin()
-            )
+        code = self.code()
 
         if not code:
             sublime.error_message('Graphviz: Please place cursor in graphviz code before running')
             return
-
 
         pdf_filename = graphvizPDF(code)
 
@@ -37,3 +29,16 @@ class GraphvizPreviewCommand(sublime_plugin.TextCommand):
             sublime.error_message('Graphviz: Could not open PDF, ' + str(e))
             raise e
 
+    def code(self):        
+        syntax = self.view.settings().get('syntax')
+        sel = self.view.sel()[0]
+
+        if syntax == DOT_SYNTAX:
+            return self.content()            
+        elif not sel.empty():
+            code = self.view.substr(sel).strip()
+        else:
+            code = surroundingGraphviz(self.content(), sel.begin())
+
+    def content(self):
+        return self.view.substr(sublime.Region(0, self.view.size()))
